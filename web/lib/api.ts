0fdc31731484a6ed7f4bc0f38236c7bf60d6bf00
sathishnagarajan <satttgns@gmail.com>
2026-01-09 00:21:06 +0530

    updated nextpublic default

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosError } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 class ApiService {
   private api: AxiosInstance;
